b67395ff70b178f4055eaadfe8c8d5e83342aacf
Johnny Bravo <HaKmonkey@gmail.com>
2023-03-18 02:31:25 -0400

    Update qiime2_manifest.py

--- bin/qiime2_manifest.py
+++ bin/qiime2_manifest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from os import listdir, path
+from os import listdir, getcwd
 
 def parse_args():
     parser = ArgumentParser()
@@ -10,21 +10,21 @@ def parse_args():
     return parser.parse_args()
 
 def direction(read):
-    if '_R1_001.fastq.gz' in read: return 0
+    if '_R1.fastq.gz' in read: return 0
     else: return 1
 
 def get_read_pairs(indir):
     read_pairs = {}
     for f in listdir(indir):
-        sample_id = f.split('_R1_001.fastq.gz')[0].split('_R2_001.fastq.gz')[0]
+        sample_id = f.split('_R1.fastq.gz')[0].split('_R2.fastq.gz')[0]
         read_pairs.setdefault(sample_id, ['',''])
-        read_pairs[sample_id][direction(f)] = path.abspath(f)
+        read_pairs[sample_id][direction(f)] = f'{getcwd()}/{indir}/{f}'
     return read_pairs
 
 def write_manifest(manifest, read_pairs):
     with open(manifest, 'w') as o:
-        o.write('sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n')
-        [o.write(f'{k}\t{read_pairs[k][0]}\t{read_pairs[k][1]}\n') for k in read_pairs]
+        o.write('sample-id,absolute-filepath,direction\n')
+        [o.write(f'{k},{read_pairs[k][0]},forward\n{k},{read_pairs[k][1]},reverse\n') for k in read_pairs]
 
 def main():
     args = parse_args()
